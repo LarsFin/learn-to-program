e5e8bdc625b404afa93f45cfaa6f1039cb7a3801
Lars <larsfinlay@googlemail.com>
2017-10-30 12:17:35 +0000

    chapter3-complete

---
+++ chapter3/Letters.rb
@@ -0,0 +1,14 @@
+#!/Usr/bin/env ruby
+puts "Ruby rocks!"
+puts "My favourite food is... " + "pizza"
+puts 5 + 5
+puts "5" + "5"
+puts "5 + 5"
+puts 1 * 4
+puts "1" * 4
+puts "1 * 4"
+#Things to avoid;
+#--Multiplying strings by strings
+#--Adding numbers to strings
+#When using '' for strings to us ' for punctuation use \'
+puts 'They\'re cool'
