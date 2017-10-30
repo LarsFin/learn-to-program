961548c90021bac71cba9e687f2a8ea61bdbdbe2
Lars <larsfinlay@googlemail.com>
2017-10-30 12:05:25 +0000

    chapter2-complete

---
+++ chapter2/ex.rb
@@ -0,0 +1,10 @@
+#!/Usr/bin/env ruby
+a = 24 * 365
+puts "There are #{a} hours in a year!"
+b = ((60 ** 2) * 24) * (365 * 10)
+puts "There are #{b} seconds in a decade!"
+c = ((60 ** 2) * 24) * (365 * 21)
+puts "I am #{c} seconds old"
+d = 1160000000 / ((60 ** 2) * 24)
+e = d / 365
+puts "Chris is #{e} years old"
